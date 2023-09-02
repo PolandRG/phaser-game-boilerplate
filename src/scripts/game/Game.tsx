a78646b3fc3b5390a00b7b18d607111de5169809
PolandRG <121366470+PolandRG@users.noreply.github.com>
2023-09-02 12:33:25 +0200

    Michal gotowy projekt

--- src/scripts/game/Game.tsx
+++ src/scripts/game/Game.tsx
@@ -47,6 +47,43 @@ function createPhaser(dispatch: GameDispatch, initialState: GameState) {
       },
       type: Phaser.WEBGL,
       pixelArt: true,
+      physics: {
+        default: 'arcade',
+        arcade: {
+              x: 0,
+              y: 0,
+          //    width: scene.sys.scale.width,
+          //    height: scene.sys.scale.height,
+              gravity: {
+                  x: 0,
+                  y: 0
+              },
+              checkCollision: {
+                  up: true,
+                  down: true,
+                  left: true,
+                  right: true
+              },
+              customUpdate: false,
+          //    fixedStep: true,
+          //    fps: 60,
+          //    timeScale: 1,     // 2.0 = half speed, 0.5 = double speed
+          //    customUpdate: false,
+          //    overlapBias: 4,
+          //    tileBias: 16,
+          //    forceX: false,
+          //    isPaused: false,
+          //    debug: false,
+          //    debugShowBody: true,
+          //    debugShowStaticBody: true,
+          //    debugShowVelocity: true,
+          //    debugBodyColor: 0xff00ff,
+          //    debugStaticBodyColor: 0x0000ff,
+          //    debugVelocityColor: 0x00ff00,
+          //    maxEntries: 16,
+          //    useTree: true   // set false if amount of dynamic bodies > 5000
+          }
+      }
     };
 
     // store gameInstance as singleton for the app
--- src/scripts/game/GameScene.ts
+++ src/scripts/game/GameScene.ts
@@ -1,9 +1,14 @@
-import { GameDispatch, GameState } from './GameContext';
+import { ActionAddScore, ActionTakeLife, GameDispatch, GameState } from './GameContext';
 import Align from './systems/Align';
 import SoundSystem from './systems/SoundSystem';
 
 export default class GameScene extends Phaser.Scene {
   private _sounds!: SoundSystem;
+  player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+  fruit: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody | undefined;
+  gameWidth!: number; gameHeight!: number;
+  deadline!: Phaser.GameObjects.Rectangle;
 
   preload() {
     Align.init(this);
@@ -22,29 +27,175 @@ export default class GameScene extends Phaser.Scene {
     this.load.audioSprite('sounds', 'audio/sounds.json', 'audio/sounds.mp3');
   }
 
-  create(data: { dispatch: GameDispatch; initialState: GameState }) {
+  create(data: {dispatch: GameDispatch, state: GameState}) {
     // TODO: here create elements
 
     // example way to add points on UI
-    // setTimeout(() => {
-    //   data.dispatch(new ActionAddScore(10));
-    // }, 1500);
+    //setTimeout(() => {
+    // data.dispatch(new ActionAddScore(10));
+    // data.dispatch(new ActionTakeLife());
+    //}, 1500);
 
-    // other systems
+    // player & movements
+
+    this.cursors = this.input.keyboard?.createCursorKeys();
+    this.player! = this.physics.add.sprite(0, 750, 'GameAtlas').setScale(1.5);
+    this.player!.setBounce(0.1);
+    this.player!.setCollideWorldBounds(true);
+    this.player!.body.setGravityY(600)
+
+    // invisible line - collision with fruit
+
+    this.gameWidth = this.sys.game.canvas.width;
+    this.gameHeight = this.sys.game.canvas.height;
+    this.deadline = this.add.rectangle(0, this.gameHeight-1, this.gameWidth*2, 2, 0x6666ff);
+    this.physics.add.existing(this.deadline);
+    
     // initialize sounds
+
     this._sounds = new SoundSystem(this.game, 'sounds');
 
     this.scale.on(Phaser.Scale.Events.RESIZE, this.resize, this);
     this.resize();
+    // add animations to player
+
+    this.anims.create({
+      key: 'player_idle',
+      frames: this.anims.generateFrameNames('GameAtlas', {
+          start: 0,
+          end: 3,
+          zeroPad: 1,
+          prefix: 'knight/knight iso char_idle_',
+          suffix: '.png'
+      }),
+      frameRate: 2,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'player_move_left',
+      frames: this.anims.generateFrameNames('GameAtlas', {
+          start: 0,
+          end: 5,
+          zeroPad: 1,
+          prefix: 'knight/knight iso char_run left_',
+          suffix: '.png'
+      }),
+      frameRate: 6,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'player_move_right',
+      frames: this.anims.generateFrameNames('GameAtlas', {
+          start: 0,
+          end: 5,
+          zeroPad: 1,
+          prefix: 'knight/knight iso char_run right_',
+          suffix: '.png'
+      }),
+      frameRate: 6,
+      repeat: -1
+    });
+
+    // set default animation
+
+    this.player!.play('player_idle');
+    
+    // spawn first fruit
+    this.spawnFruit();
+    // this.gameOver('Game Over.');
   }
 
   resize() {}
 
   update() {
-    // TODO: here update existing elements
+    // set movement direction
+    if(this.cursors?.right.isDown) {
+      this.player!.play('player_move_right', true);
+      this.player.setVelocityX(330);
+    }
+    else if(this.cursors?.left.isDown) {
+      this.player!.play('player_move_left', true);
+      this.player.setVelocityX(-330);
+    }
+    else {
+      this.player!.play('player_idle', true);
+      this.player.setVelocityX(0);
+    }
+  }
+
+  spawnFruit() {
+    setTimeout(() => {
+    // spawn fruit
+
+    var fruitSpawnX = Phaser.Math.FloatBetween(0, this.gameWidth);
+    const fruitTable = ['Apple', 'Avocado', 'Bread', 'Brownie', 'Cheese', 'Cookie', 'MelonHoneydew', 'MelonWater', 'Peach', 'PieLemon', 'Lemon', 'Onion'];
+    const fruitName = Math.floor(Math.random() * fruitTable.length);
+
+    this.fruit = this.physics.add.sprite(fruitSpawnX,50,'GameAtlas', 'fruits/'+fruitTable[fruitName]+'.png').setScale(2).setGravityY(20).setCollideWorldBounds(true);
+
+    // colliders
+
+    this.physics.add.collider(this.player, this.fruit, this.hitPlayer, function(){}, {element: this.player, fruit: this.fruit});
+    this.physics.add.collider(this.deadline, this.fruit, this.hitGround, function(){}, {element: this.deadline, fruit: this.fruit});
+    }, 10);
+
+    // spawn again
+
+    setTimeout(() => {
+      this.spawnFruit();
+    }, 3000);
+  }
+
+  // score & life functions 
+
+  addScore(data: {dispatch: GameDispatch, state: GameState}, score: number){
+    if (data && score) {
+      data.dispatch(new ActionAddScore(score));
+    }
+  }
+
+  takeLife(data: {dispatch: GameDispatch, state: GameState}){
+    if (data) {
+      data.dispatch(new ActionTakeLife());
+    }
+  }
+  
+  // features that remove objects that have collided with the player or the ground
+
+  hitPlayer(element: any, fruit: any) {
+    if (element && fruit) {
+      fruit.setActive(false).setVisible(false);
+      fruit.destroy();
+
+      // add score
+    }
+  }
+  
+  hitGround(element: any, fruit: any) {
+      if (element && fruit) {
+        console.log('failed.');
+        fruit.setActive(false).setVisible(false);
+        fruit.destroy();
+
+        // take life
+      }
+  }
+
+  // the function is called after taking 3 lifes
+  
+  gameOver(text: string) {
+      let gameOverText = this.add.text(this.gameWidth*0.5, this.gameHeight*0.5, text.toString(), {fontFamily: 'minecraftia', fontSize: 32, color: 'black'});
+      setTimeout(() => {
+        if (gameOverText) {
+        this.game.pause();
+        console.log(text.toString());}
+    }, 50);
   }
 
   get sounds() {
     return this._sounds;
   }
+
 }
